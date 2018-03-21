516809f8b8d24fbfbaec7236a95febeb932a59c2
jessiheger <jhmheger@gmail.com>
2018-03-21 09:08:30 -0700

    Commit 2 - get even answered

--- answers.js
+++ answers.js
@@ -8,3 +8,16 @@ for (var i = 1; i <=20; i++) {
 }
 
 num();
+
+// Get Even
+// Write a for loop that will log only the even numbers in 0 through 200.
+function even() {
+	for (var i = 0; i <= 200; i++) {
+		if (i % 2 === 0) {
+			console.log(i)
+		}
+	}
+}
+
+even();
+
